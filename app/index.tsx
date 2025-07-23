830ea4cdf8775e2a02f71129c5d6b2f663b226a6
Drew Smith <drew.robert.smith@gmail.com>
2025-07-23 08:21:10 -0500

    added basic station stream page

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,36 @@
-import { View } from "react-native";
+import { useAppTheme } from "@/components/ui/theme-provider";
+import { Image } from "expo-image";
+import { Text, View } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function Index() {
-  return <View className="flex-1"></View>;
+  const xnLogo = require("../assets/images/splash-icon.png");
+  const { colors } = useAppTheme();
+
+  return (
+    <View className="flex-1 justify-between items-center">
+      <View className="items-center justify-evenly">
+        <Image
+          source={xnLogo}
+          contentFit="contain"
+          cachePolicy="memory-disk"
+          style={{
+            aspectRatio: 1,
+            height: "60%",
+            borderRadius: 999,
+            borderWidth: 1,
+            borderColor: colors.border,
+          }}
+        />
+        <Text className="text-lg" style={{ color: colors.text }}>
+          Live Stream Metadata
+        </Text>
+        <MaterialIcons
+          name="play-circle-filled"
+          size={88}
+          color={colors.secondary}
+        />
+      </View>
+    </View>
+  );
 }
