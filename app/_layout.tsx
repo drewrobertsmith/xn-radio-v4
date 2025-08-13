db1038c08a37a8074f70c3451ddd590947c06ded
Drew Smith <drew.robert.smith@gmail.com>
2025-08-13 12:44:13 -0500

    hide profile layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -108,6 +108,7 @@ export default function RootLayout() {
                   <Tabs.Screen
                     name="profile"
                     options={{
+                      href: null, //hide route for now
                       title: "Profile",
                       tabBarIcon: ({ color }) => (
                         <MaterialIcons
